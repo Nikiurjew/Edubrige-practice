b74e59170119347601ed99c4328c928cda4c9715
Nikita jha <86097451+Nikiurjew@users.noreply.github.com>
2023-11-25 00:24:33 +0530

    Update Pattern10.java

--- Pattern/Pattern10.java
+++ Pattern/Pattern10.java
@@ -1,10 +1,15 @@
 package Pattern;
 
+import java.util.Scanner;
+
 public class Pattern10 {
 
 	public static void main(String[] args) {
-		int a=4;
-		for(int i = 1;i<=4;i++) {
+		//int a=4;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number:");
+		int n=sc.nextInt();
+		for(int i = 1;i<=n;i++) {
 			for(int j=3;j>=i;j--) {
 				System.out.print(" ");
 			}
